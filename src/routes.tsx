0452f6af0f9fe1fbdec4f4aa5ce7ac8350ee71ed
Daniel <ddazaj@uninorte.edu.co>
2021-09-22 12:00:35 -0500

    fix lazy load

--- src/routes.tsx
+++ src/routes.tsx
@@ -11,10 +11,12 @@ const Page404 = React.lazy(() => import("@pages/404/"));
 
 const AppRoutes = (): JSX.Element => (
     <Default>
-        <Switch>
-            <Route path='/' exact component={Home} />
-            <Route component={Page404} />
-        </Switch>
+        <React.Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+                <Route path='/' exact component={Home} />
+                <Route component={Page404} />
+            </Switch>
+        </React.Suspense>
     </Default>
 );
 
